09fd0b04807cc1b00417fdffc664b6985ba45126
Harefx <harefx.dev@gmail.com>
2021-05-31 00:51:40 +0700

    add devise and user, category model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-class HomeController < ApplicationController
+class HomeController < BaseController
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index; end
 end
