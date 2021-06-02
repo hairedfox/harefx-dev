a418324b4ce46a7b1895f34df8fceca67d58f0af
Harefx <harefx.dev@gmail.com>
2021-06-02 23:39:43 +0700

    add simple UI for login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: "", path_names: {
+    sign_in: "login",
+    sign_out: "logout"
+  }
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
